33a3f3f9edd4b980d9e4feac566a4446baee0b65
Brandon Salazar <alu.18131281@correo.itlalaguna.edu.mx>
2022-12-12 00:02:26 -0600

    Medio jala

--- app/src/main/java/mx/edu/itl/c18131289/apporganizadordefotos/ImagenActivity.java
+++ app/src/main/java/mx/edu/itl/c18131289/apporganizadordefotos/ImagenActivity.java
@@ -2,12 +2,16 @@ package mx.edu.itl.c18131289.apporganizadordefotos;
 
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,6 +26,7 @@ import modelo.ImageAdapter;
 public class ImagenActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<Image> arrayList = new ArrayList<>();
+    String categoriaSeleccionada;
     private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(),
             result -> {
                 if (result) {
@@ -36,6 +41,8 @@ public class ImagenActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(ImagenActivity.this));
         recyclerView.setHasFixedSize(true);
 
+        categoriaSeleccionada = this.getIntent().getStringExtra("categoria");
+
         if (ActivityCompat.checkSelfPermission(ImagenActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         } else if (ActivityCompat.checkSelfPermission(ImagenActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -48,12 +55,49 @@ public class ImagenActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        getImages();
+        NoImages();
+     //   getImages();
+    }
+    public void NoImages(){
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()
+                + File.separator
+                + "DCIM"
+                + File.separator
+                + "Organizador"
+                + File.separator
+                + categoriaSeleccionada
+                + File.separator;
+        File file = new File(filePath);
+        File[] files = file.listFiles();
+        if (files.length == 0) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Este directorio esta vacio")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                         finish();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+
+        }
+        else
+        {
+            getImages();
+        }
     }
 
     private void getImages(){
         arrayList.clear();
-        String filePath = "/storage/emulated/DCIM/Organizador/Amigos/";
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()
+                + File.separator
+                + "DCIM"
+                + File.separator
+                + "Organizador"
+                + File.separator
+                + categoriaSeleccionada
+                + File.separator;
         File file = new File(filePath);
         File[] files = file.listFiles();
         if (files != null) {
@@ -61,6 +105,7 @@ public class ImagenActivity extends AppCompatActivity {
                 if (file1.getPath().endsWith(".png") || file1.getPath().endsWith(".jpg")) {
                     arrayList.add(new Image(file1.getName(), file1.getPath(), file1.length()));
                 }
+
             }
         }
         ImageAdapter adapter = new ImageAdapter(ImagenActivity.this, arrayList);
